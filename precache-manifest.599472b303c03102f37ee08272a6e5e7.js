73020532365222b3242da4d24c68556577c54091
Rocio Avila <rocio.avila@northwestern.com>
2020-09-30 23:31:38 -0500

    Updates

---
+++ precache-manifest.599472b303c03102f37ee08272a6e5e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17a797288582fc0b27fc60704e57b6a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe982b8cb037957aaab",
+    "url": "/static/css/main.d6c096ba.chunk.css"
+  },
+  {
+    "revision": "c328fbf00e71c80a15b3",
+    "url": "/static/js/2.d5c7b837.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5c7b837.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe982b8cb037957aaab",
+    "url": "/static/js/main.a27590e2.chunk.js"
+  },
+  {
+    "revision": "1a9355e74d03dc4c7068",
+    "url": "/static/js/runtime-main.e0225f3c.js"
+  }
+]);
